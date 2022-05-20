f15defb0c0da8518686338577ccec05e500ee1a4
Casey Spence <orangobang@gmail.com>
2022-05-20 13:53:28 -0400

    seperated into respective files for game engine and game contents

--- gameMods.py
+++ gameMods.py
@@ -1,14 +1,11 @@
 from sys import exit
 from random import randint
 from textwrap import dedent
+import skyrim
 
-#Basics classes to start game and make scenes
-class Scene(object):
-
-    def enter(self):
-        print('Scene has not been completed')
-        exit(1)
-
+#game runner. taking scene_map as a Map() object argument
+# play() sets current_scene as a map object and calls opening_scene
+# and next_scene.
 class Engine(object):
 
     def __init__(self, scene_map):
@@ -22,17 +19,21 @@ class Engine(object):
             next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
 
-#The game runner that will reference all of the above scenes.
+        #print out last scene
+        current_scene.enter()
+
+#Contains a dict of scenes defined in skyrim.py and we give it the starting
+# scene as an argument. next_scene() returns the class assigned in the dict
 class Map(object):
 
     scenes = {
-        'wagon': WagonRide(),
-        'training': TrainginGround(),
-        'whiterun': Whiterun(),
-        'bandits': BanditFight(),
-        'dragon': DragonFight(),
-        'death': Death(),
-        'end': End(),
+        'wagon': skyrim.WagonRide(),
+        'training': skyrim.TraingingGround(),
+        'whiterun': skyrim.Whiterun(),
+        'bandits': skyrim.BanditFight(),
+        'dragon': skyrim.DragonFight(),
+        'death': skyrim.Death(),
+        'end': skyrim.End(),
     }
 
     def __init__(self, start_scene):
@@ -42,5 +43,9 @@ class Map(object):
         val = Map.scenes.get(scene_name)
         return val
 
-    def opening_scene(self, start_scene):
+    def opening_scene(self):
         return self.next_scene(self.start_scene)
+
+a_map = Map('wagon')
+a_game = Engine(a_map)
+a_game.play()
--- skyrim.py
+++ skyrim.py
@@ -1,37 +1,49 @@
-import gameMods.py
 
-#Sceens and main game play
+#Basics classes to start game and make scenes
+class Scene(object):
+
+    def enter(self):
+        print('Scene has not been completed')
+        exit(1)
+
 class WagonRide(Scene):
 
     def enter(self):
-        print('TODO')
+        print('WagonScene')
+        return 'training'
 
-class TrainginGround(Scene):
-
-    def enter(self):
-        print('TODO')
-
-class WhiteRun(Scene):
+class TraingingGround(Scene):
 
     def enter(self):
-        print('TODO')
+        print('Traiging')
+        return 'whiterun'
+
+class Whiterun(Scene):
+
+    def enter(self):
+        print('Whiterun')
+        return 'bandits'
 
 class BanditFight(Scene):
 
     def enter(self):
-        print('TODO')
+        print('Bandit')
+        return 'dragon'
 
 class DragonFight(Scene):
 
     def enter(self):
-        print('TODO')
+        print('Dragon')
+        return 'death'
 
 class Death(Scene):
 
     def enter(self):
-        print('TODO')
+        print('Death')
+        exit(1)
 
 class End(Scene):
 
     def enter(self):
-        print('TODO')
+        print('End')
+        return 'end'
